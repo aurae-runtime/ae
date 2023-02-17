1764fe3186f0f804e83bcf69fac22cc8fac3f3c9
dominic <510002+dmah42@users.noreply.github.com>
2023-02-17 08:50:41 +0000

    rename runtime to cells to match aurae (#56)
    * rename runtime to cells to match aurae
    
    * update package name too

--- cmd/cells/cells.go
+++ cmd/cells/cells.go
@@ -28,13 +28,17 @@
  *                                                                            *
 \* -------------------------------------------------------------------------- */
 
-package stop
+package cells
 
 import (
 	"context"
 	"io"
 
 	aeCMD "github.com/aurae-runtime/ae/cmd"
+	allocate "github.com/aurae-runtime/ae/cmd/cells/allocate"
+	free "github.com/aurae-runtime/ae/cmd/cells/free"
+	start "github.com/aurae-runtime/ae/cmd/cells/start"
+	stop "github.com/aurae-runtime/ae/cmd/cells/stop"
 	"github.com/aurae-runtime/ae/pkg/cli"
 	"github.com/spf13/cobra"
 )
@@ -54,7 +58,7 @@ func (o *option) Validate() error {
 }
 
 func (o *option) Execute(_ context.Context) error {
-	return o.outputFormat.ToPrinter().Print(o.writer, "stop called")
+	return o.outputFormat.ToPrinter().Print(o.writer, "cells called")
 }
 
 func (o *option) SetWriter(writer io.Writer) {
@@ -64,14 +68,26 @@ func (o *option) SetWriter(writer io.Writer) {
 func NewCMD(ctx context.Context) *cobra.Command {
 	o := &option{}
 	cmd := &cobra.Command{
-		Use:   "stop",
-		Short: "Stop a runtime resource.",
-		Long:  `Stop a runtime resource.`,
+		Use:   "cells",
+		Short: "CLI for aurae cells service.",
+		Long:  `CLI for aurae cells service.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return aeCMD.Run(ctx, o, cmd, args)
 		},
 	}
-	// add flags here
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cmd.AddCommand(allocate.NewCMD(ctx))
+	cmd.AddCommand(free.NewCMD(ctx))
+	cmd.AddCommand(start.NewCMD(ctx))
+	cmd.AddCommand(stop.NewCMD(ctx))
 
 	return cmd
 }
